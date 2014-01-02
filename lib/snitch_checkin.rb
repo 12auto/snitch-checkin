1e9867d7647048deadf02a1248b2b6e6ff63e533
Marcos Essindi <me@12auto.de>
2014-01-02 17:00:22 +0100

    Fixed typo

--- lib/snitch_checkin.rb
+++ lib/snitch_checkin.rb
@@ -2,7 +2,7 @@ require "snitch_checkin/version"
 
 module SnitchCheckin
   
-  def self.check_in_snitch(snitch_name, messsage)
+  def self.check_in_snitch(snitch_name, message)
     snitches = YAML.load_file Rails.root.join("config", "snitches.yml")
     `curl -d "m=#{message}" #{snitches["#{snitch_name}"]}` if snitches
   end
