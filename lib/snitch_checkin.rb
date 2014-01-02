c4fae8fdfd35f50219948932b56d5df4ca3ccd1c
Marcos Essindi <me@12auto.de>
2014-01-02 17:46:51 +0100

    Optional message

--- lib/snitch_checkin.rb
+++ lib/snitch_checkin.rb
@@ -2,9 +2,10 @@ require "snitch_checkin/version"
 
 module SnitchCheckin
   
-  def self.check_in_snitch(snitch_name, message)
+  def self.check_in_snitch(snitch_name, message = nil)
     snitches = YAML.load_file Rails.root.join("config", "snitches.yml")
-    `curl -d "m=#{message}" #{snitches["#{snitch_name}"]}` if snitches
+    message_option = "-d 'm=#{message}'" if message
+    `curl #{message_option} #{snitches["#{snitch_name}"]}` if snitches
   end
 
 end
