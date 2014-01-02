a7f943f6ab2210e4c38784fdf9a3de405fb69a2a
Marcos Essindi <me@12auto.de>
2014-01-02 17:49:46 +0100

    No need to process the message if there are no snitches

--- lib/snitch_checkin.rb
+++ lib/snitch_checkin.rb
@@ -4,8 +4,9 @@ module SnitchCheckin
   
   def self.check_in_snitch(snitch_name, message = nil)
     snitches = YAML.load_file Rails.root.join("config", "snitches.yml")
+    return unless snitches
     message_option = "-d 'm=#{message}'" if message
-    `curl #{message_option} #{snitches["#{snitch_name}"]}` if snitches
+    `curl #{message_option} #{snitches["#{snitch_name}"]}`
   end
 
 end
